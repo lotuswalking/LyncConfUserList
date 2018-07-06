1a8ae2843da98e1a51e9e24d9b56e2e11012b503
lijy1 <lijy1@lenovo.com>
2018-07-06 14:01:01 +0800

    this is first version

---
+++ LyncConfUserList/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace LyncConfUserList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.conbox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.datagv1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Export = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.datagv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Conversation List:";
+            // 
+            // conbox1
+            // 
+            this.conbox1.FormattingEnabled = true;
+            this.conbox1.Location = new System.Drawing.Point(157, 34);
+            this.conbox1.Name = "conbox1";
+            this.conbox1.Size = new System.Drawing.Size(235, 20);
+            this.conbox1.TabIndex = 1;
+            this.conbox1.SelectedIndexChanged += new System.EventHandler(this.conbox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(408, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GetUserList";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // datagv1
+            // 
+            this.datagv1.AllowUserToAddRows = false;
+            this.datagv1.AllowUserToDeleteRows = false;
+            this.datagv1.AllowUserToResizeColumns = false;
+            this.datagv1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.datagv1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.datagv1.Location = new System.Drawing.Point(39, 72);
+            this.datagv1.Name = "datagv1";
+            this.datagv1.ReadOnly = true;
+            this.datagv1.RowTemplate.Height = 23;
+            this.datagv1.Size = new System.Drawing.Size(729, 370);
+            this.datagv1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(543, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Export
+            // 
+            this.Export.Location = new System.Drawing.Point(642, 34);
+            this.Export.Name = "Export";
+            this.Export.Size = new System.Drawing.Size(75, 23);
+            this.Export.TabIndex = 5;
+            this.Export.Text = "Export";
+            this.Export.UseVisualStyleBackColor = true;
+            this.Export.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.timer1.Enabled = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 459);
+            this.Controls.Add(this.Export);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.datagv1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.conbox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.datagv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox conbox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView datagv1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Export;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ LyncConfUserList/Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Data.OleDb;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Lync.Model;
+using Microsoft.Lync.Model.Conversation;
+using Microsoft.Lync.Model.Extensibility;
+
+namespace LyncConfUserList
+{
+
+    //{33F41092-47E5-43BD-97AC-1EFA7A6B9F33}
+    public partial class Form1 : Form
+    {
+       // EndPointInformation endPointInformation;   //定义EndPoint对象
+
+        public LyncClient _Client;
+
+        public Microsoft.Lync.Model.Conversation.ConversationManager _ConversationMgr;
+        //private InstantMessageModality _LocalIMModality;
+        public Microsoft.Lync.Model.Conversation.Conversation _LycConversation;
+        public Boolean _InitializeFlag;
+        public ConversationWindow _conversationWindow;
+        public string CurrentUserName;
+        private BindingSource bindingSource1 = new BindingSource();
+        bool isExported;
+        bool needUpdated;
+        DataTable dtConv;
+        DataTable dtPar;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeConList();
+        }
+
+        private void InitializeConList()
+        {
+            try
+            {
+
+
+                _Client = LyncClient.GetClient();
+                SignInConfiguration sc = _Client.SignInConfiguration;
+                if (sc != null)
+                {
+                    CurrentUserName = sc.UserName.ToString();
+                }
+                CurrentUserName = _Client.Uri.ToString().Replace("sip:", "");
+                _ConversationMgr = _Client.ConversationManager;
+                _ConversationMgr.ConversationAdded += ConversationAdd;
+                _ConversationMgr.ConversationRemoved += ConversationRemoved;
+                conbox1.Items.Add("Pls Select A Item");
+                dtConv = new DataTable();
+                dtConv.Columns.Add("DisplayName", typeof(string));
+                dtConv.Columns.Add("Id", typeof(string));
+                foreach (Microsoft.Lync.Model.Conversation.Conversation c in _ConversationMgr.Conversations)
+                {
+                    string DisplayName = "";
+
+                    foreach (Participant p in c.Participants)
+                    {
+                        if (!p.IsSelf)
+                        {
+                            DisplayName += ", " + p.Properties[ParticipantProperty.Name].ToString();
+                            // conbox1.Items.Add(p.Properties[ParticipantProperty.Name].ToString());
+                        }
+                    }
+                    if (DisplayName.Length > 3)
+                    {
+                        DisplayName = DisplayName.Substring(2);
+                    }
+                    else
+                    {
+                        DisplayName = "My Self";
+                    }
+
+                    dtConv.Rows.Add(DisplayName, c.Properties[ConversationProperty.Id].ToString());
+                    
+                    //conbox1.Items.Add(c.Properties[ConversationProperty.Id].ToString());
+                }
+                conbox1.DataSource = dtConv;
+                conbox1.DisplayMember = "DisplayName";
+                conbox1.ValueMember = "Id";
+                needUpdated = true;
+                //datagv1.DataBindings();
+                datagv1.DataSource = bindingSource1;
+                //conbox1.Update();
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+         }
+
+        private void ConversationRemoved(object sender, ConversationManagerEventArgs e)
+        {
+            
+            Conversation c = e.Conversation;
+            //foreach(DataRow row in dtConv.Rows)
+            for(int i=0;i<dtConv.Rows.Count;i++)
+            {
+                DataRow row = dtConv.Rows[i];
+                if(row["Id"].ToString() == c.Properties[ConversationProperty.Id].ToString())
+                {
+                    dtConv.Rows.Remove(row);
+                }
+            }
+            needUpdated = true;
+            dtConv.AcceptChanges();
+           
+        }
+
+        private void ConversationAdd(object sender, ConversationManagerEventArgs e)
+        {
+            string DisplayName = "";
+            Conversation c = e.Conversation;
+
+            foreach (Participant p in c.Participants)
+            {
+                if (!p.IsSelf)
+                {
+                    DisplayName += ", " + p.Properties[ParticipantProperty.Name].ToString();
+                    // conbox1.Items.Add(p.Properties[ParticipantProperty.Name].ToString());
+                }
+            }
+            if (DisplayName.Length > 3)
+            {
+                DisplayName = DisplayName.Substring(2);
+            }
+            else
+            {
+                DisplayName = "My Self";
+            }
+
+            dtConv.Rows.Add(DisplayName, c.Properties[ConversationProperty.Id].ToString());
+            dtConv.AcceptChanges();
+            needUpdated = true;
+
+            //dtConv.Rows.Add();
+        }
+
+        private void conbox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           // MessageBox.Show(conbox1.SelectedValue.ToString());
+              
+        }
+
+        private string GetNow()
+        {
+            String strFileName = string.Format("{0:yyyy-MM-dd HH:mm:ss}", DateTime.Now);
+            return strFileName;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (conbox1.SelectedIndex==-1)
+            {
+                MessageBox.Show("Please select Any Conversation first");
+                return;
+            }
+            String ConId = conbox1.SelectedValue.ToString();
+            //dtConv.Columns.Add("Id", typeof(string));
+            dtPar = new DataTable();
+            dtPar.Columns.Add("Displayname", typeof(string));
+            dtPar.Columns.Add("IsAnonymous", typeof(string));
+            dtPar.Columns.Add("JoinTime", typeof(string));
+            dtPar.Columns.Add("LeftTime", typeof(string));
+            isExported = false;
+            foreach (Microsoft.Lync.Model.Conversation.Conversation c in _ConversationMgr.Conversations)
+            {
+                if(c.Properties[ConversationProperty.Id].ToString() == ConId)
+                {
+                    _LycConversation = c;
+                    _LycConversation.ParticipantAdded += ParticipantAdd;
+                    _LycConversation.ParticipantRemoved += ParticipantRemoved;
+                    foreach (Participant p in c.Participants)
+                    {
+                        if((Boolean)p.Properties[ParticipantProperty.IsAuthenticated])
+                        dtPar.Rows.Add(p.Properties[ParticipantProperty.Name].ToString(), "No", GetNow(), "");
+                        else
+                            dtPar.Rows.Add(p.Properties[ParticipantProperty.Name].ToString(), "Yes", GetNow(), "");
+                    }
+
+                }
+            }
+            //datagv1.BindingContext(dtPar);
+            bindingSource1.DataSource = dtPar;
+            datagv1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            
+            button1.Enabled = false;
+            button2.Enabled = true;
+
+          }
+        
+
+        private void ParticipantRemoved(object sender, ParticipantCollectionChangedEventArgs e)
+        {
+            foreach (DataRow dr in dtPar.Rows)
+            {
+                if (dr["Displayname"].ToString() == e.Participant.Properties[ParticipantProperty.Name].ToString())
+                {
+                    dr["LeftTime"] = GetNow();
+                    //Console.WriteLine("Left Time:%s", dr["LeftTime"]);
+                }
+            }
+
+            dtPar.AcceptChanges();
+            needUpdated = true;
+            //dtPar.
+        }
+
+        private void ParticipantAdd(object sender, ParticipantCollectionChangedEventArgs e)
+        {
+            //e.Participant
+            Participant p = e.Participant;
+
+            foreach (DataRow dr in dtPar.Rows)
+            {
+                if (dr["Displayname"].ToString() == p.Properties[ParticipantProperty.Name].ToString())
+                {
+                    dr["JoinTime"] = GetNow();
+                    dtPar.AcceptChanges();
+                    bindingSource1.DataSource = dtPar;
+                    datagv1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                    return;
+                }
+            }
+            if ((Boolean)p.Properties[ParticipantProperty.IsAuthenticated])
+                dtPar.Rows.Add(p.Properties[ParticipantProperty.Name].ToString(), "No", GetNow(), "");
+            else
+                dtPar.Rows.Add(p.Properties[ParticipantProperty.Name].ToString(), "Yes", GetNow(), "");
+
+            needUpdated = true;
+
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(isExported)
+            {
+                button1.Enabled = true;
+                button2.Enabled = false;
+                timer1.Enabled = false;
+            }else
+            {
+                MessageBox.Show("Please export User List Before Stop Listen");
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String strName = string.Format("{0:yyyyMMddHHMMss}", DateTime.Now);
+            string fpath = "LyncConfUserlist" + strName;
+            ExtractDataToCSV(datagv1);
+        }
+        private void ExtractDataToCSV(DataGridView dgv)
+        {
+
+            // Don't save if no data is returned
+            if (dgv.Rows.Count == 0)
+            {
+                return;
+            }
+            StringBuilder sb = new StringBuilder();
+            // Column headers
+            string columnsHeader = "";
+            for (int i = 0; i < dgv.Columns.Count; i++)
+            {
+                columnsHeader += dgv.Columns[i].Name + ",";
+            }
+            sb.Append(columnsHeader + Environment.NewLine);
+            // Go through each cell in the datagridview
+            foreach (DataGridViewRow dgvRow in dgv.Rows)
+            {
+                // Make sure it's not an empty row.
+                if (!dgvRow.IsNewRow)
+                {
+                    for (int c = 0; c < dgvRow.Cells.Count; c++)
+                    {
+                        // Append the cells data followed by a comma to delimit.
+
+                        sb.Append(dgvRow.Cells[c].Value + ",");
+                    }
+                    // Add a new line in the text file.
+                    sb.Append(Environment.NewLine);
+                }
+            }
+            // Load up the save file dialog with the default option as saving as a .csv file.
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "CSV files (*.csv)|*.csv";
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // If they've selected a save location...
+                using (System.IO.StreamWriter sw = new System.IO.StreamWriter(sfd.FileName, false))
+                {
+                    // Write the stringbuilder text to the the file.
+                    sw.WriteLine(sb.ToString());
+                    isExported = true;
+                    MessageBox.Show("CSV file saved.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("You Didn't Saved List");
+            }
+            // Confirm to the user it has been completed.
+            
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (needUpdated)
+            {
+                conbox1.DataSource = null;
+                conbox1.Items.Clear();
+                conbox1.DataSource = dtConv;
+                conbox1.DisplayMember = "DisplayName";
+                conbox1.ValueMember = "Id";
+                datagv1.Update();
+                datagv1.Refresh();
+                needUpdated = false;
+            }
+           
+        }
+    }
+}
